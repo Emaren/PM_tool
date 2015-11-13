bdd9c09551183d5fcad0e2bd1c8b2c7bedf66d60
Tony Blum <tonyblum@me.com>
2015-11-12 16:53:00 -0800

    built 4 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  get "home"        => "home#index"
+  get "/home"        => "home#index"
   get "/home/about" => "home#about"
+
+  root "home#index"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
